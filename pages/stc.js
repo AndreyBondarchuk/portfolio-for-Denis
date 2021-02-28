d1f85feffac1b07e717f0abaa2139b78ba1428d2
andrey <AndreyBondarchuk@users.noreply.github.com>
2021-02-28 21:37:09 +0200

    site was created

---
+++ pages/stc.js
@@ -0,0 +1,39 @@
+let burger = document.querySelector('#burger');
+let clic = document.querySelector('.menu_adap');
+let crest = document.querySelector('#crest');
+let main = document.querySelector('.main');
+let about = document.querySelector('#aboutA')
+let skills = document.querySelector('#skillsA')
+let portfolio = document.querySelector('#portfolioA')
+let cont = document.querySelector('#contactsA')
+let head = document.querySelector('#headA');
+
+
+burger.onclick = () => {
+  clic.classList.toggle('click');
+  main.classList.toggle('not_bg');
+}
+crest.onclick = () => {
+  clic.classList.toggle('click');
+  main.classList.toggle('not_bg');
+}
+about.onclick = () => {
+  clic.classList.toggle('click');
+  main.classList.toggle('not_bg');
+}
+skills.onclick = () => {
+  clic.classList.toggle('click');
+  main.classList.toggle('not_bg');
+}
+portfolio.onclick = () => {
+  clic.classList.toggle('click');
+  main.classList.toggle('not_bg');
+}
+cont.onclick = () => {
+  clic.classList.toggle('click');
+  main.classList.toggle('not_bg');
+}
+head.onclick = () => {
+  clic.classList.toggle('click');
+  main.classList.toggle('not_bg');
+}
